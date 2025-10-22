eb9454e3af4a631ccb3aae5da7aee5bec7442134
Nacho Puricelli <nachopuricelli87@gmail.com>
2025-10-22 00:36:50 -0300

    Prueba link seccion

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,18 @@
 import { Container, Navbar } from 'react-bootstrap'
 import { Nav } from 'react-bootstrap'
-
-
+ import { Link, Element } from 'react-scroll';
 
 const Header = () => {
   return (
     <>
-     
-    <Navbar expand="lg" data-bs-spy="scroll" data-bs-target="#seccionHistoria" data-bs-offset="0" className="bg-gradient shadow-lg">
+    <Navbar expand="lg" className="bg-gradient shadow-lg">
       <Container>
         <Navbar.Brand href="#Inico" className="fw-bolder text-primary NEmpresa">Nombre de tu Empresa</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mx-auto fw-semibold" >
             <Nav.Link id='NavLink' href="#Inicio">Inicio</Nav.Link>
-            <Nav.Link id='NavLink' href="#seccionHistoria">Historia</Nav.Link>
+            <Nav.Link id='NavLink' href="#Historia">Historia</Nav.Link>
             <Nav.Link id='NavLink' href="#Testimonios">Testimonios</Nav.Link>
             <Nav.Link id='NavLink' href="#Contacto">Contacto</Nav.Link>
           </Nav>
@@ -24,5 +22,14 @@ const Header = () => {
     </>
   )
 }
+
+   <><Element name="seccionHistoria">
+  
+</Element><Link to="seccionHistoria" smooth={true} duration={500}>
+   
+  </Link></>
+
+
+
 
 export default Header
